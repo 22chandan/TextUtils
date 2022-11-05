c0c505f5dff4c7d04759f115f2be8c2fe5e92804
chandan kumar <101493083+22chandan@users.noreply.github.com>
2022-11-05 12:15:59 +0530

    Completed

--- src/Components/About.js
+++ src/Components/About.js
@@ -45,7 +45,7 @@ export default function About(props) {
                   color: props.mode === "dark" ? "white" : "#042743",
                 }}
               >
-                What is TextUtils?
+                <strong>What is TextUtils?</strong>
               </button>
             </h2>
             <div
@@ -84,7 +84,7 @@ export default function About(props) {
                   color: props.mode === "dark" ? "white" : "#042743",
                 }}
               >
-                Accordion Item #2
+                <strong>Free to Use</strong>
               </button>
             </h2>
             <div
@@ -103,15 +103,10 @@ export default function About(props) {
                   color: props.mode === "dark" ? "white" : "#042743",
                 }}
               >
-                <strong>This is the second item's accordion body.</strong> It is
-                hidden by default, until the collapse plugin adds the
-                appropriate classes that we use to style each element. These
-                classes control the overall appearance, as well as the showing
-                and hiding via CSS transitions. You can modify any of this with
-                custom CSS or overriding our default variables. It's also worth
-                noting that just about any HTML can go within the{" "}
-                <code>.accordion-body</code>, though the transition does limit
-                overflow.
+                TextUtils is a free character counter tool that provides instant
+                character count & word count statistics for a given text.
+                TextUtils reports the number of words and characters. Thus it is
+                suitable for writing text with word/character limit.
               </div>
             </div>
           </div>
@@ -129,7 +124,7 @@ export default function About(props) {
                   color: props.mode === "dark" ? "white" : "#042743",
                 }}
               >
-                Accordion Item #3
+                <strong> Browser Compatible</strong>
               </button>
             </h2>
             <div
@@ -144,15 +139,10 @@ export default function About(props) {
                   color: props.mode === "dark" ? "white" : "#042743",
                 }}
               >
-                <strong>This is the third item's accordion body.</strong> It is
-                hidden by default, until the collapse plugin adds the
-                appropriate classes that we use to style each element. These
-                classes control the overall appearance, as well as the showing
-                and hiding via CSS transitions. You can modify any of this with
-                custom CSS or overriding our default variables. It's also worth
-                noting that just about any HTML can go within the{" "}
-                <code>.accordion-body</code>, though the transition does limit
-                overflow.
+                This word counter software works in any web browsers such as
+                Chrome, Firefox, Internet Explorer, Safari. It suits to count
+                characters in facebook, blog, books, excel document, pdf
+                document, essays etc.
               </div>
             </div>
           </div>
